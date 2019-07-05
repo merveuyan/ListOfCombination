a03ddf0714c204291542626a177c80c9c649cb02
merveuyan <50906959+merveuyan@users.noreply.github.com>
2019-07-05 19:44:48 +0300

    CreateCombinationTest methodu eklendi.

---
+++ ListOfCombination/ListOfCombinationTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ListOfCombination
+{
+    public class Tests
+    {
+        //[Fact]
+        //public static List<int> GetNumbersTest()
+        //{
+        //    List<int> numListTest = new List<int> { 1, 2, 3, 4, 5, 6 };
+        //    List<int> actual = new List<int>();
+        //    actual = Program.GetNumbers();
+        //    Assert.Equal(numListTest, actual);
+        //    return actual;
+        //}
+
+        [Fact]
+        public void CreateCombinationsTest()
+        {
+            List<int> testInput = new List<int>{1,2,3,4,5,6};
+            List<string>expected =new List<string> {"1,2,3,4,5,6"};
+            List<string> actual = Program.CreateCombinations(testInput);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ ListOfCombinationTest/ProgramTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace ListOfCombinationTest
+{
+    public class ProgramTest
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        [Fact]
+        public static List<int> GetNumbersTest()
+        {
+
+        }
+    }
+}
